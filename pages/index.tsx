976624746cc9044343262745b0f2b6595fa17b8a
Alejandro <alejandro.deleon@utp.ac.pa>
2020-12-12 23:59:40 -0500

    eliminating css files and changing js to ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import "../styles/index.css";  // applied everywhere in the Next.js application scope
+
 
 export default function Home() {
   return (
